272355c8d268836a334c04d4212542b6fc1ab18e
unknown <yeasalehofficial2002@gmail.com>
2022-08-25 23:04:08 +0600

    string

---
+++ jsStringCuttingStringReplacementCommonUsed.js
@@ -0,0 +1,23 @@
+// ------------------------------ String start ----------------------------------------
+/* 
+ 1. string ar modde jodi 2 tai double court """" thake or 2 ta single court '''' thake , tobe seta program syntax error dibe. ai problem theke somadhan paile courtation ar agy \ use korte hobe. tobe ar syntax error dibe na. like ; "\"\"" or '\'\''
+*/
+
+let myName = "Yeasaleh";
+let myCountry = "I live in Bangladesh";
+let myCountryWithCort1 = "I live in 'Bangladesh'";
+let myCountryWithCort2 = 'I live in "Bangladesh"';
+let myCountryWithCort3 = 'I live in \'Bangladesh\'';
+let myCountryWithCort4 = "I live in \"Bangladesh\"";
+
+console.log(myName);
+console.log(myCountry);
+console.log(myCountryWithCort1);
+console.log(myCountryWithCort2);
+console.log(myCountryWithCort3);
+console.log(myCountryWithCort4);
+
+
+
+
+// ------------------------------ String start ----------------------------------------
